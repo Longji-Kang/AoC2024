335855d9504ffe7f4d57279b9d2f904cdda4fd5f
Longji Kang <kanglongjidev@gmail.com>
2025-01-14 17:45:18 +0200

    Add day 1 and day 2 solutions

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -2,14 +2,73 @@
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
+    private List<string> _firstSet;
+    private List<string> _secondSet;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    private void GenerateSets() {
+        _firstSet = new List<string>();
+        _secondSet = new List<string>();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        const string delimiter = "   ";
+
+        foreach (var input in _input) {
+            string[] sets = input.Split(delimiter);
+
+            _firstSet.Add(sets[0]);
+            _secondSet.Add(sets[1]);
+        }
+    }
+
+    private string SolveProblem1(string[] input) {
+        GenerateSets();
+
+        int sumTotal = 0;
+
+        int iterations = _firstSet.Count;
+
+        for (int i = 0; i < iterations; i++) {
+            string firstMin = _firstSet.Min();
+            string secondMin = _secondSet.Min();
+
+            int firstMinIndex = _firstSet.IndexOf(firstMin);
+            int secondMinIndex = _secondSet.IndexOf(secondMin);
+
+            _firstSet.RemoveAt(firstMinIndex);
+            _secondSet.RemoveAt(secondMinIndex);
+
+            int distance = Math.Abs(Convert.ToInt32(firstMin) - Convert.ToInt32(secondMin));
+
+            sumTotal += distance;
+        }
+
+        return sumTotal.ToString();
+    }
+
+    private string SolveProblem2(string[] input) {
+        GenerateSets();
+
+        int sumTotal = 0;
+
+        int iterations = _firstSet.Count;
+
+        foreach (string curr in _firstSet) {
+            int count = _secondSet.Count(s => s == curr);
+
+            int weight = Convert.ToInt32(curr) * count;
+
+            sumTotal += weight;
+        }
+
+        return sumTotal.ToString();
+    }
+
+    public override ValueTask<string> Solve_1() => new(SolveProblem1(_input));
+
+    public override ValueTask<string> Solve_2() => new(SolveProblem2(_input));
 }
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+    public class Day02 : BaseDay {
+        private string[] _input;
+
+        private List<List<int>> _matrix = new List<List<int>>();
+
+        public Day02() {
+            _input = File.ReadAllLines(InputFilePath);
+        }
+
+        private void CreateMatrixFromInput(string[] input) {
+            _matrix = new List<List<int>>();
+
+            for (int i = 0; i < input.Length; i++) {
+                string[] curr_split = input[i].Split(' ');
+
+                _matrix.Add(new List<int>());
+                for (int j = 0; j < curr_split.Length; j++) {
+                    _matrix[i].Add(Convert.ToInt32(curr_split[j]));
+                }
+            }
+        }
+
+        private string SolveFirst(string[] input) {
+            CreateMatrixFromInput(input);
+
+            int safe = 0;
+
+            for (int i = 0; i < _matrix.Count; i++) {
+                bool currIsSafe = true;
+
+                bool increments = true;
+
+                for (int j = 0; j < _matrix[i].Count - 1; j++) {
+                    int diff = _matrix[i][j + 1] - _matrix[i][j];
+
+                    if (j == 0) {
+                        if (diff < 0) {
+                            increments = false;
+                        }
+                    }
+
+                    if (diff < 0) {
+                        if (increments == true) {
+                            currIsSafe = false;
+                            break;
+                        }
+                    } else if (diff > 0) {
+                        if (increments == false) {
+                            currIsSafe = false;
+                            break;
+                        }
+                    } else if (diff == 0) {
+                        currIsSafe = false;
+                        break;
+                    }
+
+                    if (Math.Abs(diff) > 3) { 
+                        currIsSafe = false; 
+                        break; 
+                    }
+                }
+
+                if (currIsSafe) {
+                    safe += 1;
+                }
+            }
+
+            return safe.ToString();
+        }
+
+        private string SolveSecond(string[] input) {
+            int safe = 0;
+
+            CreateMatrixFromInput(input);
+
+            for (int i = 0; i < _matrix.Count; i++) {
+                int increments = 0;
+                int decrements = 0;
+                int zeroDiff = 0;
+
+                for (int j = 0; j < _matrix[i].Count - 1; j++) {
+                    if (_matrix[i][j + 1] - _matrix[i][j] > 0) {
+                        increments++;
+                    } else if (_matrix[i][j + 1] - _matrix[i][j] < 0) {
+                        decrements++;
+                    } else {
+                        zeroDiff++;
+                    }
+                }
+
+                if (zeroDiff > 1) {
+                    continue;
+                }
+
+                if (CheckList(_matrix[i], increments > decrements)) {
+                    safe += 1;
+                } else {
+                    // Make copy of list
+                    List<int> tmp = new List<int>(_matrix[i]);
+
+                    bool isSafe = false;
+
+                    for (int j = 0; j < tmp.Count - 2; j++) {
+                        int nextDiff = tmp[j + 1] - tmp[j];
+
+                        if (nextDiff == 0 || (nextDiff > 3 || nextDiff < -3) || (nextDiff > 0 && increments < decrements) || (nextDiff < 0 && increments > decrements)) {
+                            List<int> remTmp = new List<int>(tmp);
+
+                            // Try remove current
+                            remTmp.RemoveAt(j);
+
+                            if (CheckList(remTmp, increments > decrements)) {
+                                safe++;
+                                isSafe = true;
+                                break;
+                            } else {
+                                remTmp = new List<int>(tmp);
+                                remTmp.RemoveAt(j + 1);
+
+                                if (CheckList(remTmp, increments > decrements)) {
+                                    safe++;
+                                    isSafe = true;
+                                    break;
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    if (!isSafe) {
+                        int indexA = _matrix[i].Count - 2;
+                        int indexB = _matrix[i].Count - 1;
+
+                        tmp = new List<int>(_matrix[i]);
+
+                        tmp.RemoveAt(indexA);
+
+                        if (CheckList(tmp, increments > decrements)) {
+                            safe++;
+                        } else {
+                            tmp = new List<int>(_matrix[i]);
+
+                            tmp.RemoveAt(indexB);
+
+                            if (CheckList(tmp, increments > decrements)) {
+                                safe++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return safe.ToString();
+        }
+
+        private bool CheckList(List<int> list, bool increments) {
+            for (int j = 0; j < list.Count - 1; j++) {
+                int diff = list[j + 1] - list[j];
+
+                if (diff < 0) {
+                    if (increments == true) {
+                        return false;
+                    }
+                } else if (diff > 0) {
+                    if (increments == false) {
+                        return false;
+                    }
+                } else if (diff == 0) {
+                    return false;
+                }
+
+                if (Math.Abs(diff) > 3) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override ValueTask<string> Solve_1() => new(SolveFirst(_input));
+
+        public override ValueTask<string> Solve_2() => new(SolveSecond(_input));
+    }
+}
